235c4877c85e61e2ab5a05e96ee8cdd92d5a4dfc
jamfox <karl-andreas7@hotmail.com>
2021-09-12 20:40:33 +0300

    Fixed encoder driver init and moved everything to keymap.c

---
+++ jamtoqmk.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Copying jam to ~/qmk_firmware/keyboards/sofle/keymaps/"
+cp -r jam ~/qmk_firmware/keyboards/sofle/keymaps/
+echo "After copying:"
+ls -lart ~/qmk_firmware/keyboards/sofle/keymaps/
+
---
+++ qmktojam.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "Copying jam from ~/qmk_firmware/keyboards/sofle/keymaps/"
+cp -r ~/qmk_firmware/keyboards/sofle/keymaps/jam .
+echo "After copying:"
+ls -lart 
+
